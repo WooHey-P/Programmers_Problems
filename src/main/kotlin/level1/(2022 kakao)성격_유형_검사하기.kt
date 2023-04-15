6030813007d56702ed24c9c2cac258c62da256e2
WooHyun-P <dkqnwlnd@naver.com>
2023-04-15 12:44:11 +0900

    개인정보 문제만 읽어봄

---
+++ src/main/kotlin/level1/(2022 kakao)성격_유형_검사하기.kt
@@ -0,0 +1,16 @@
+package level1
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/118666
+ * */
+
+fun solution(survey: Array<String>, choices: IntArray): String {
+    var answer: String = ""
+    return answer
+}
+
+
+fun main() {
+    println(solution(survey = arrayOf("AN", "CF", "MJ", "RT", "NA"), choices = intArrayOf(5, 3, 2, 7, 5)))  // T C M A
+    println(solution(survey = arrayOf("TR", "RT", "TR"), choices = intArrayOf(7, 1, 3)))    // R C J A
+}
